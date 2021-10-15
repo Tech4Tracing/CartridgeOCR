d4ea1ae426352b204ee20e06499647c8935e90f4
Diana Sullivan <disulliv@microsoft.com>
2021-10-14 17:06:34 -0700

    Add prediction api

--- src/webapp/CartridgeOCRApp/src/pages/Predict.tsx
+++ src/webapp/CartridgeOCRApp/src/pages/Predict.tsx
@@ -1,12 +1,36 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { camera } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+  IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+  IonCol, IonImg, IonActionSheet, IonCard, IonCardHeader,
+  IonCardTitle, IonCardSubtitle, IonCardContent, IonListHeader } from '@ionic/react';
+import { usePredictionApi } from '../hooks/usePredictionApi';
 import './Predict.css';
 
 const Predict: React.FC = () => {
+  const { prediction, getPrediction } = usePredictionApi();
   return (
     <IonPage>
       <IonContent fullscreen>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardSubtitle>CartridgeOCR</IonCardSubtitle>
+            <IonCardTitle>Get prediction results</IonCardTitle>
+          </IonCardHeader>
 
-        <h3>To be implemented</h3>
+          <IonCardContent>
+            Upload an image to the prediction API to see the results. Click the camera button to take a photo & upload.
+          </IonCardContent>
+        </IonCard>
+
+        <IonCard>
+          <IonImg src={ prediction.webviewPath } />
+        </IonCard>
+
+        <IonFab vertical="bottom" horizontal="center" slot="fixed">
+          <IonFabButton onClick={() => getPrediction()}>
+            <IonIcon icon={camera}></IonIcon>
+          </IonFabButton>
+        </IonFab>
 
       </IonContent>
     </IonPage>
