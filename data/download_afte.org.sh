775b0e2009d54af0afb68360a6132b9ecb95177f
chris <chris@inferno>
2021-12-29 09:45:27 +1100

    add requirements.txt and data download script

---
+++ data/download_afte.org.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+wget \
+     --level=inf \
+     --limit-rate=20K \
+     --recursive \
+     --page-requisites \
+     --user-agent=Mozilla \
+     --no-parent \
+     --convert-links \
+     --adjust-extension \
+     https://afte.org/laravel.php/gallery/index
+
+#--wait=2 \
+#     --no-clobber \
+#     -e robots=off \
+     
