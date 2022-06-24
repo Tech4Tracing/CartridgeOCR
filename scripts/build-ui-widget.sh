d1b207a1ac420e5372caaf5c149d0008345d614c
Arkadiy Korotaev <131850+koriaf@users.noreply.github.com>
2022-06-24 23:28:00 +0200

    fix(build): Copy manifest.json file when building the widget (#83)

---
+++ scripts/build-ui-widget.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+rm -rf /app/annotation/annotations_app/static/js /app/annotation/annotations_app/static/css /app/annotation/annotations_app/templates/ui/index.html
+cd /app/t4t-annotation-UI
+yarn install
+yarn build
+cp /app/t4t-annotation-UI/build/index.html /app/annotation/annotations_app/templates/ui/
+cp -R /app/t4t-annotation-UI/build/static/css /app/t4t-annotation-UI/build/static/js /app/t4t-annotation-UI/build/manifest.json /app/annotation/annotations_app/static/
