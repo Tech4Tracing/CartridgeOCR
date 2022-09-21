0134be97c931433f5e0b405151eebb32a701039b
edkazcarlson <edkazcarlson@gmail.com>
2022-09-21 15:47:48 -0700

    add submodule

--- src/snakeModel/training/train.py
+++ src/snakeModel/training/train.py
@@ -7,19 +7,18 @@ textSnakeModelWeightsPath = 'textsnake/textsnake_r50_fpn_unet_1200e_ctw1500-27f6
 downloadUrlBase = 'https://download.openmmlab.com/mmocr/textdet/'
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def train():
+def train(model, criterion, epochs):
+    
 
 
 def loadModel(modelName = 'textsnake'):
-
-
-def main(modelName = 'textsnake'):
-    
     if modelName == 'textsnake':
         det_ckpt = f'{downloadUrlBase}{textSnakeModelWeightsPath}'
         detect_model = init_detector(
-            det_config, det_ckpt, device=device)
-        detect_model = revert_sync_batchnorm(self.detect_model)
+            'src\snakeModel\config\textsnake_r50_fpn_unet_1200e_ctw1500.py', det_ckpt, device=device)
+        return revert_sync_batchnorm(detect_model)
     else:
         print('Unsupported model')
         exit()
+
+def main(modelName = 'textsnake'):
