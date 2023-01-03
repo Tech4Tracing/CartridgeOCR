290ac85a4ee6cad991bb202265bc48e12d13c114
Robert Sim <robert.sim@gmail.com>
2023-01-02 20:51:42 -0800

    Migrate to redis and add ui deployment script (#113)

---
+++ scripts/build-ui-deployment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+rm -rf src/annotation/annotations_app/static/js src/annotation/annotations_app/static/css src/annotation/annotations_app/templates/ui/index.html
+pushd src/t4t-annotation-UI
+yarn install
+yarn build
+popd
+cp src/t4t-annotation-UI/build/index.html src/annotation/annotations_app/templates/ui/
+cp -R src/t4t-annotation-UI/build/static/css src/t4t-annotation-UI/build/static/js src/t4t-annotation-UI/build/manifest.json src/annotation/annotations_app/static/
